3dc6324f755532b1f53c10a9ee620a30605c317e
Raphael Onofre <raphael.onofre@gmail.com>
2019-08-07 17:32:04 -0300

    Set a header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react'
+import { Container, Menu, Image } from 'semantic-ui-react'
+
 
 function App() {
   return (
-    <div>
-      <Button primary>Primary</Button>
-      <Button secondary>Secondary</Button>
-    </div>
+      <Menu fixed='top' inverted>
+      <Container>
+        <Menu.Item as='a' header>
+          <Image size='mini' style={{ marginRight: '1.5em' }} />
+          Fareja Despesas - Câmara Federal
+        </Menu.Item>
+      </Container>
+    </Menu>
   );
 }
 
