9976b8107f047e02e84779fba4480019e89b4fe0
Raphael Onofre <raphael.onofre@gmail.com>
2019-08-26 14:13:17 -0300

    fix: set link on header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import './Header.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-                <div className="navbar-item">
-                    <div className="title is-4 header-is-white">DEPUTADOS DO BRASIL</div>
-                </div>
+  return (
+    <Link to="/">
+      <nav
+        className="navbar is-dark"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="navbar-brand">
+          <div className="navbar-item">
+            <div className="title is-4 header-is-white">
+              DEPUTADOS DO BRASIL
             </div>
-        </nav>
-    );
-}
+          </div>
+        </div>
+      </nav>
+    </Link>
+  );
+};
 
-export default Header
+export default Header;
