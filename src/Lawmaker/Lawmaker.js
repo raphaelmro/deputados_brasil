a7f43ff1e7f2241a83a2a70bff7644c694f36534
Raphael Onofre <raphael.onofre@gmail.com>
2019-08-30 16:49:41 -0300

    fix: remove card class from lawmaker div

--- src/Lawmaker/Lawmaker.js
+++ src/Lawmaker/Lawmaker.js
@@ -6,13 +6,11 @@ function Lawmaker({ id, name, avatar, partyAcronym, FUAcronym }) {
   return (
     <div className="column is-one-fifth">
       <Link to={`/deputado/detalhes/${id}`}>
-        <div className="card">
           <div className="card-image">
-            <figure className="image">
+            <figure className="image figure-box">
               <img className="foto-deputado" src={avatar} alt={name} />
             </figure>
           </div>
-        </div>
         <div className="content content-deputado">
             <p>{name} - {partyAcronym}/{FUAcronym}</p>
         </div>
